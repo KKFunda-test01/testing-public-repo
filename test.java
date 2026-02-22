688366f9e6b0f6acf463a64e85db5079405914c1
chandrasekhar-aalvaru <chandrasekhar4149@gmail.com>
2026-02-22 12:25:16 +0530

    Jira-002:Updated Test.java file

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class Test{
 	public static void main(String[] args){
 		System.out.println("Hi all, this is sample java program");
+		System.out.println("This is updated line to test the -a option along with git commit command for modified file");
 	}
 }
